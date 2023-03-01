fba7ce87758f97b911b502455ecde0a2a07ee7c1
royeplg <roye.p@gmail.com>
2023-03-01 11:15:03 +0200

    add lest milking

--- server.js
+++ server.js
@@ -7,40 +7,23 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-// const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+let lestDayMilking = [];
 
-// async function from_google() {
-//   const auth = await google.auth.getClient({
-//     scopes: SCOPES,
-//   });
-//   const sheets = google.sheets({ version: "v4", auth });
-//   const response = await sheets.spreadsheets.values.get({
-//     spreadsheetId: "1NMjBbIzVdMCgDgBet7aSeukOLHQz_GIyveDSEPAd9K4",
-//     range: "Sheet1", // Change this to the range you want to fetch
-//   });
-//   console.log(response);
-// }
-
-// from_google();
-app.get("/", (req, res) => {
-  // Handle the update from the Google Sheets file
-  console.log(req.body);
-
-  res.send(req.body);
-  // res.sendStatus(200);
+app.get("/test", (req, res) => {
+  res.sendStatus(200);
 });
-app.post("/updates", (req, res) => {
-  // Handle the update from the Google Sheets file
-  console.log(req.body);
-
-  res.send(req.body);
+app.get("/get_lest_milking", (req, res) => {
+  res.send(lestDayMilking);
   // res.sendStatus(200);
+  lestDayMilking = [];
+});
+
+app.post("/updates", (req, res) => {
+  console.log(req.body);
+  lestDayMilking.push(req.body);
+  res.send(req.body);
 });
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
-
-// 77.137.1.114:3000 ip out
-
-// 172.20.0.47:3000
